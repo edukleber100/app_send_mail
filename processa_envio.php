e6dbec27636863b6587a1b11f9611929ba26f651
edukleber100 <edukleber100@gmail.com>
2023-11-02 17:29:34 -0300

    melhora no feedback visual

--- processa_envio.php
+++ processa_envio.php
@@ -14,9 +14,10 @@
             private $para = null;
             private $assunto = null;
             private $mensagem = null;
+            public $status = array( 'codigo_status' => null , 'descricao_status' => '');
 
             public function __get($atributo) {
-                return this-> $atributo;
+                return $this-> $atributo;
             }
 
             public function __set($atributo, $valor){
@@ -42,7 +43,7 @@
         //print_r($mensagem);
         if (!$mensagem->mensagemValida()) {
             echo 'Mensagem não é válida';
-            die();
+            header('Location: index.php');
         } 
 
 
@@ -51,37 +52,91 @@
 
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->SMTPDebug = false;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'user@example.com';                     //SMTP username
-            $mail->Password   = 'secret';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Username   = 'phpcurso280@gmail.com';                     //SMTP username
+            $mail->Password   = 'ezsl waso gfaf mzbl';                               //SMTP password
+            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-            $mail->addAddress('ellen@example.com');               //Name is optional
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
+            $mail->setFrom('phpcurso280@gmail.com', 'Email Teste2 Rementente');
+            $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
 
             //Attachments
-            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = $mensagem->__get('assunto');
+            $mail->Body    = $mensagem->__get('mensagem');
+            //$mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
 
             $mail->send();
-            echo 'Message has been sent';
+
+            $mensagem->status['codigo_status'] = 1;
+            $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso!';
+
         } catch (Exception $e) {
-            echo "Não foi possível enviar este e-mail! Por favor tente novamente mais tarde.";
-            echo '<br />';
-            echo 'Detalhes do erro: ' . $mail->ErrorInfo;
+
+            $mensagem->status['codigo_status'] = 2;
+            $mensagem->status['descricao_status'] = 'Não foi possível enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;
+
         }
+?>
+
+
+<html>
+    <head>
+            <meta charset="utf-8" />
+            <title>App Mail Send</title>
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    <?php if($mensagem->status['codigo_status'] == 1) { ?>
+
+                            <div class="container">
+                                <h1 class="display-4 text-success">Sucesso</h1>
+                                <p><?= $mensagem->status['descricao_status'] ?></p>
+                                <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                            </div>
+
+                    <?php } ?>
+
+                    <?php if($mensagem->status['codigo_status'] == 2) { ?>
+
+                            <div class="container">
+                                <h1 class="display-4 text-danger">Ops!</h1>
+                                <p><?= $mensagem->status['descricao_status'] ?></p>
+                                <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                            </div>
+
+                    <?php } ?>
+
+
+                </div>
+            </div>
+            
+
+        </div>
+
+    </body>
+</html>
